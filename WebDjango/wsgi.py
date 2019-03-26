b2f0614ba030dd8ff5977ae16a682a96a2a4e1c6
Alexey Delov <aleksejdelov@gmail.com>
2019-03-26 23:46:25 +0300

    req

--- WebDjango/wsgi.py
+++ WebDjango/wsgi.py
@@ -10,8 +10,7 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebDjango.settings')
+
 application = get_wsgi_application()
-application = WhiteNoise(application)
