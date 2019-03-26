18ad01a1b58006fd644d23cb6cc7b2bda18ba3c8
Alexey Delov <aleksejdelov@gmail.com>
2019-03-26 14:03:59 +0300

    req

--- WebDjango/urls.py
+++ WebDjango/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from univer.views import db
+
 urlpatterns = [
+path('db/', db),
     path('admin/', admin.site.urls),
 ]
