b8a154e1d1d0bdfee9eed7080e056930ec2481a0
Alexey Delov <aleksejdelov@gmail.com>
2019-03-26 22:12:25 +0300

    req

--- univer/models.py
+++ univer/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Dostopr(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    rate = models.FloatField()
