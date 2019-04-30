7a491bded44b74c016470c2a14ea6b9b0fd6701d
Alexey Delov <aleksejdelov@gmail.com>
2019-04-30 13:54:33 +0300

    QQQ

--- database/views.py
+++ database/views.py
@@ -13,7 +13,7 @@ def postg(request):
 
 def jsdb(request):
     sort = request.GET.get("sort", "")
-    b = Dostopr.objects.values().order_by(sort)
+    b = Dostopr.objects.values().order_by(sort, "")
     list_result = [entry for entry in b]
     return JsonResponse(list_result, safe=False)
 
