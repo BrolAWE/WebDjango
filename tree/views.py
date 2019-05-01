9337214fae05fa81ebd3fd18d5044b042aeb0533
Alexey Delov <aleksejdelov@gmail.com>
2019-05-01 19:58:36 +0300

    QQQ

--- tree/views.py
+++ tree/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 
 #
+from tree.models import Workshop
+
 
 def tree(request):
     workshops = Workshop.objects.all()
-    return render(request, "tree.html")
+    return render(request, "tree.html", {"workshops": workshops})
