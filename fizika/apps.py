238ceb4afea8db290f6ca35145c1b56578c1ff39
Alexey Delov <aleksejdelov@gmail.com>
2019-05-01 14:16:24 +0300

    QQQ

---
+++ fizika/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FizikaConfig(AppConfig):
+    name = 'fizika'
---
+++ fizika/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def fizika1(request):
+    return render(request, "fizika1.html")
