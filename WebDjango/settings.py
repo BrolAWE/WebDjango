163ac128f2a649e7aea0ea6eed9b8ed3847a2f28
Alexey Delov <aleksejdelov@gmail.com>
2019-05-05 10:54:19 +0300

    QQQ

--- WebDjango/settings.py
+++ WebDjango/settings.py
@@ -43,7 +43,10 @@ INSTALLED_APPS = [
     'univer',
     'channels',
     'database',
+    'crispy_forms'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
